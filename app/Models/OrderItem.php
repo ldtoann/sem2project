4c135eb703569439eaf2b28ec2da5aa16c6592b1
vanduxx11 <vandu8515@gmail.com>
2023-07-17 18:13:37 +0700

    model relationship

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+    protected $fillable = ['order_id', 'quantity','product_id','pricetotal'];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+    public function order(){
+        return $this->belongsTo(Order::class);
+    }
 }
